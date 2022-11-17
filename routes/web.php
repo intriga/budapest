db7e1532a3bbf6ff7968aaf5240fbbc7b1d3bbaf
1ntr1g4 <intriga2402@gmail.com>
2022-11-17 12:29:41 -0400

    login changes

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\Backend\DashboardController::class, 'index'])->name('admin');
 
 Auth::routes();
 
